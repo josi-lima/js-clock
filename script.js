9e72beb9818169b33e568e91c85064646ed2a362
josi-lima <teacher.josi@yahoo.com>
2022-09-18 19:16:01 -0300

    update css and js

--- script.js
+++ script.js
@@ -28,3 +28,31 @@ const nameDivs = () => {
   document.getElementsByClassName('hand')[2].classList.add('second-hand');
 }
 nameDivs();
+
+// function to get the current date and time for the analog clock
+
+const secondHand = document.getElementsByClassName('hand')[2];
+const minHand = document.getElementsByClassName('hand')[1];
+const hourHand = document.getElementsByClassName('hand')[0];
+
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const minutes = now.getMinutes();
+  const minsDegrees = ((minutes / 60) * 360) + ((seconds/60)*6) + 90;
+  minHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegrees = ((hours / 12) * 360) + ((mins / 60)*30) + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+}
+
+setInterval(setDate, 1000);
+
+setDate();
+
