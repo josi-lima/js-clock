3409d755c05a5e7ee00378ef26dc392448be6898
josi-lima <teacher.josi@yahoo.com>
2022-09-18 23:20:08 -0300

    update html, css and js

--- script.js
+++ script.js
@@ -29,13 +29,15 @@ const nameDivs = () => {
 }
 nameDivs();
 
-// function to get the current date and time
+// Variables for the analog clock
 
 const secondHand = document.getElementsByClassName('hand')[2];
 const minHand = document.getElementsByClassName('hand')[1];
 const hourHand = document.getElementsByClassName('hand')[0];
 
-function setDate() {
+// function to get the current date and time - ANALOG CLOCK
+
+function setClockDate() {
   const now = new Date();
 
   const seconds = now.getSeconds();
@@ -43,15 +45,37 @@ function setDate() {
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
   const minutes = now.getMinutes();
-  const minsDegrees = ((minutes / 60) * 360) + ((seconds/60)*6) + 90;
+  const minsDegrees = ((minutes / 60) * 360) + ((seconds/60) * 6) + 90;
   minHand.style.transform = `rotate(${minsDegrees}deg)`;
 
   const hours = now.getHours();
-  const hoursDegrees = ((hours / 12) * 360) + ((minutes / 60)*30) + 90;
+  const hoursDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
-setInterval(setDate, 1000);
+setInterval(setClockDate, 1000);
 
-setDate();
+setClockDate();
 
+// function to get the current date and time - DIGITAL CLOCK
+
+function setDigitalTime() {
+
+  const date = new Date();
+
+  let dSec = date.getSeconds();
+  let dMin = date.getMinutes();
+  let dHour = date.getHours();
+
+  dSec = (dSec < 10) ? '0' + dSec : dSec;
+  dMin = (dMin < 10) ? '0' + dMin : dMin;
+  dHour = (dHour < 10) ? '0' + dHour : dHour;
+   
+  const time = `${dHour}:${dMin}:${dSec}`;
+
+  document.getElementById('digital').textContent = time;
+}
+
+setInterval(setDigitalTime, 1000);
+
+setDigitalTime();
